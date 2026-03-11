c74596ad3e0a0536789c67c72ad73f7b03f64483
l1senochek <lisenochek96@gmail.com>
2026-03-11 15:18:22 +0300

    feat: router changed to createBrowserRoute

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import { TaskPage } from 'pages/tasks/ui/TaskPage';
 
-export function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<TaskPage />} />
-    </Routes>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <TaskPage />,
+  },
+]);
