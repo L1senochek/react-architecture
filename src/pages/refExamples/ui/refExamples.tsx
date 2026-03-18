869a532652aab51f8229c37c54ad4eb0d069b362
l1senochek <lisenochek96@gmail.com>
2026-03-18 16:22:26 +0300

    feat: add ClickTimer

---
+++ src/pages/refExamples/ui/refExamples.tsx
@@ -0,0 +1,11 @@
+import { ClickTimer } from 'features/refExamples/ClickTimer';
+
+import type { RefExampleItem } from '../model/types';
+
+export const REF_EXAMPLES: RefExampleItem[] = [
+  {
+    id: 'click-timer',
+    title: '1) Компонент ClickTimer:',
+    content: <ClickTimer />,
+  },
+];
