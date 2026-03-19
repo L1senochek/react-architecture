52470c3834da539e06728cd1b725fe9d34453b27
l1senochek <lisenochek96@gmail.com>
2026-03-19 16:17:50 +0300

    feat: update Input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,9 +1,10 @@
 import type { InputProps } from './types';
 import styles from './Input.module.css';
 
-export function Input({ className, hasError, inputSize, ...props }: InputProps) {
+export function Input({ className, hasError, inputSize, variant = 'dark', ...props }: InputProps) {
   const classes = [
     styles.input,
+    variant === 'white' ? styles.white : '',
     inputSize === 'sm' ? styles.sm : '',
     hasError ? styles.error : '',
     className ?? '',
