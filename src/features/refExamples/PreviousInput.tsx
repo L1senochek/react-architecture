8de5d3809a78817ede2ff6571c1274ced051d2e2
l1senochek <lisenochek96@gmail.com>
2026-03-19 16:20:40 +0300

    feat: add PreviousInput

---
+++ src/features/refExamples/PreviousInput.tsx
@@ -0,0 +1,41 @@
+import { type ChangeEvent, useEffect, useRef, useState } from 'react';
+
+import styles from './previousInput.module.css';
+import { Input } from 'shared/ui/Input';
+
+export function PreviousInput() {
+  const [value, setValue] = useState('');
+  const previousValueRef = useRef('');
+  const [previousValue, setPreviousValue] = useState('');
+
+  useEffect(() => {
+    setPreviousValue(previousValueRef.current);
+    previousValueRef.current = value;
+  }, [value]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.previousInput}>
+      <label className={styles.label}>
+        <span>Введите текст:</span>
+        <Input
+          id="previous-input"
+          type="text"
+          inputSize="sm"
+          value={value}
+          placeholder="Введите текст"
+          variant="white"
+          onChange={handleChange}
+        />
+      </label>
+
+      <div className={styles.previousValue}>
+        Предыдущее значение:
+        <span className={styles.previousValueText}>{previousValue || '-'}</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/refExamples/ui/refExamples.tsx
+++ src/pages/refExamples/ui/refExamples.tsx
@@ -1,4 +1,5 @@
 import { ClickTimer } from 'features/refExamples/ClickTimer';
+import { PreviousInput } from 'features/refExamples/PreviousInput';
 
 import type { RefExampleItem } from '../model/types';
 
@@ -7,5 +8,10 @@ export const REF_EXAMPLES: RefExampleItem[] = [
     id: 'click-timer',
     title: '1) Компонент ClickTimer:',
     content: <ClickTimer />,
+  },
+  {
+    id: 'previous-input',
+    title: '2) Компонент PreviousInput:',
+    content: <PreviousInput />,
   },
 ];
